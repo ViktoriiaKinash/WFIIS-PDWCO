03dad27302c624d6546c34d5da570d1ecc05cd3e
ViktoriiaKinash <viktoriiakinash@gmail.com>
2023-12-20 22:18:33 +0100

    Change uris

--- backend/config.js
+++ backend/config.js
@@ -3,9 +3,9 @@ const neo4j = require('neo4j-driver');
 
 const uri = process.env.URI
 
-const user = 'neo4j'
+const user = process.env.USER
 
-const password = 'PASSword123fisagh'
+const password = process.env.PASSWORD
 const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
 
 module.exports.driver = driver;
